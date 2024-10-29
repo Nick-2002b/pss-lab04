ce79283220ad28f0747b7d384b91800ab6ac1100
Nick-2002b <nicolae.balaban@studio.unibo.it>
2024-10-29 10:11:13 +0100

    Modify catV2.java: Restyle code

---
+++ 00-qa/src/main/java/IT/UNIBO/qa/catV2.java
@@ -0,0 +1,52 @@
+package IT.UNIBO.qa;
+
+public class catV2 {
+    private static int nine = 9;
+    static public int numberOfLives = nine;
+
+    public String Name;
+    public String Breed;
+    public String Color;
+    public int Age;
+
+    public catV2(String meow, final String color, final int age, final String korat){
+        this.Name = meow;
+        this.Color = color;
+        this.Age = age;
+        this.Breed = korat;
+    }
+
+    public String Meow(){
+        return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!"; 
+    }
+    
+    void setColor(String color){
+        this.Color = color;
+    }
+
+    public String getColor(){
+        return this.Color;
+    }
+
+    public int hasSameColor(catV2 cat){
+        if(this.Color == cat.Color){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public String getName(){
+        return this.Name;
+    }
+
+    public String getBreed(){
+        return this.Breed;
+    }
+
+    public static void main(String[] args) {
+        catV2 cat1 = new catV2("Kiri", "Gray", 4, "korat");
+
+        System.out.println(cat1.Meow());
+    }
+}
