fe95feb1bdbc0d99735a972e0b75a7bedf4e154c
Nick-2002b <nicolae.balaban@studio.unibo.it>
2024-10-29 11:54:43 +0100

    Edit file Kangaroo.java: Implement of canEat() and canEatVegetables() methods

--- 02-interface-composition/src/main/java/it/unibo/Kangaroo.java
+++ 02-interface-composition/src/main/java/it/unibo/Kangaroo.java
@@ -5,5 +5,15 @@ public class Kangaroo implements Animal {
     public double averageWeight() {
         return 55.0;
     }
+
+    @Override
+    public boolean canEatVegetable() {
+        return true;
+    }
+
+    @Override
+    public boolean canEat(Animal a) {
+        return this.averageWeight() > a.averageWeight();
+    }
     
 }
