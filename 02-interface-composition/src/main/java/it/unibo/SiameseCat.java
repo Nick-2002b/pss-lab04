7e807f2805abc4414b10b4e9e5d2a5e1c69fa036
Nick_2002b <nicolae.balaban@studio.unibo.it>
2024-11-03 00:35:35 +0100

    Edit SiameseCat.java: Use of Diet & refactory of canEat method

--- 02-interface-composition/src/main/java/it/unibo/SiameseCat.java
+++ 02-interface-composition/src/main/java/it/unibo/SiameseCat.java
@@ -2,18 +2,25 @@ package it.unibo;
 
 public class SiameseCat implements Animal {
 
+    private Diet dieta;
+    SiameseCat(){
+        this.dieta = new Carnivore();
+    }
     public double averageWeight() {
         return 4.8;
     }
 
     @Override
     public boolean canEatVegetables() {
-        return false;
+        return dieta.isCarnivore();
     }
 
     @Override
     public boolean canEat(Animal a) {
-        return this.averageWeight() > a.averageWeight();
+        if(this.dieta.isCarnivore() && this.averageWeight() > a.averageWeight()) {
+            return true;
+        }
+        return false;
     }
     
 }
